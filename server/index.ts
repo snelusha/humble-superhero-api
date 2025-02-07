cb7311b549bf50c916e70fab0328aca126878bfa
snelusha <hello@snelusha.dev>
2025-02-08 02:31:48 +0530

    feat: add superheroes endpoints with validation

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,47 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 
+import { z } from "zod";
+
+const superheroSchema = z.object({
+  name: z.string(),
+  superpower: z.string(),
+  humility: z.number().int().min(1).max(10),
+});
+
+type SuperPower = z.infer<typeof superheroSchema>;
+
+const superheroes: SuperPower[] = [];
+
 const app = new Hono();
+
+const api = app.basePath("/api");
+
+api.get("/superheroes", (c) => {
+  const sortedSuperheroes = [...superheroes].sort(
+    (a, b) => b.humility - a.humility
+  );
+
+  return c.json(sortedSuperheroes);
+});
+
+api.post("/superheroes", async (c) => {
+  try {
+    const superhero = superheroSchema.parse(await c.req.json());
+
+    if (superheroes.some((s) => s.name === superhero.name)) {
+      return c.json({ error: "Superhero already exists" }, 400);
+    }
+
+    superheroes.push(superhero);
+
+    return c.json(superhero);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return c.json("Invalid superhero data", 400);
+    }
+  }
+});
 
 serve(
   {
